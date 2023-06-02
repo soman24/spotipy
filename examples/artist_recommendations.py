c7bc4dbc75299872b8401a409bd2e195887602db
Soman Khan <76926660+soman24@users.noreply.github.com>
2023-06-02 18:52:43 +0100

    Update artist_recommendations.py
    Added function to retrieve related artists

--- examples/artist_recommendations.py
+++ examples/artist_recommendations.py
@@ -28,6 +28,13 @@ def get_artist(name):
         return None
 
 
+def get_related_artists(artist_id):
+    results = sp.artist_related_artists(artist_id)
+    related_artists = [artist['name'] for artist in results['artists']]
+    return related_artists
+
+
+
 def show_recommendations_for_artist(artist):
     results = sp.recommendations(seed_artists=[artist['id']])
     for track in results['tracks']:
